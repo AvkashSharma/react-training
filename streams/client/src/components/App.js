cb0fad7df732c254679b76f4c9a944d2e4e5f6a3
Avkash <avkaashsharma89@gmail.com>
2020-07-08 01:34:05 -0400

    Project Init and Component Scaffolding

---
+++ streams/client/src/components/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const PageOne = () => {
+	return (
+		<div>
+			PageOne
+			<Link to="/pagetwo"> Navigate to Page two </Link>
+		</div>
+	);
+};
+
+const PageTwo = () => {
+	return (
+		<div>
+			PageTwo
+			<button>Click Me!</button>
+		</div>
+	);
+};
+
+const App = () => {
+	return (
+		<div>
+			<BrowserRouter>
+				<div>
+					<Route path="/" exact component={PageOne} />
+					<Route path="/pagetwo" component={PageTwo} />
+				</div>
+			</BrowserRouter>
+		</div>
+	);
+};
+
+export default App;
